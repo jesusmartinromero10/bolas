2f724aa8ab0a95f41f7e384592d5f9e7d4036970
Jesus Martin <jesusmartinromero10@gmail.com>
2022-08-03 18:46:02 +0200

    trabajo verano echo

--- bolas.py
+++ bolas.py
@@ -1,3 +1,4 @@
+from random import randint
 import pygame as pg
 
 
@@ -15,8 +16,16 @@ class Cuadrado:
     def velocidad(self, vx, vy):
         self.vx = vx
         self.vy = vy
-    def mover(self):
+
+    def mover(self, xmax, ymax):
         self.x += self.vx
+        self.y += self.vy
+
+        if self.x <= 0 or self.x >= xmax - self.w:
+            self.vx *= -1
+        
+        if self.y <= 0 or self.y >= ymax - self.h:
+            self.vy *= -1
 
         
 
@@ -25,9 +34,20 @@ pg.init()
 pantalla_principal = pg.display.set_mode((800, 600))
 pg.display.set_caption("Bolitas rebotando")
 
-cuadrado = Cuadrado(400, 300, color = (255, 255, 0))
-cuadrado2 = Cuadrado(400, 300, 35, 35, (0, 255, 0))
 
+bolas = 0
+cuadradi = []
+while bolas <= randint(3, 1000):
+    cuadrao = Cuadrado(400, 300, color= (randint(0, 255), randint(0, 255), randint(0, 255)))
+    cuadradi.append(cuadrao) 
+    bolas += 1
+
+#cuadrado = Cuadrado(400, 300, color = (255, 255, 0))
+#cuadrado.velocidad(5, 5)
+#cuadrado2 = Cuadrado(300, 300, 35, 35, (0, 255, 0))
+#cuadrado2.velocidad(randint(-10, 10), randint(-10, 10))
+for h in range(len(cuadradi)):
+    cuadradi[h].velocidad(randint(-10, 10), randint(-10, 10))
 
 game_over = False
 while not game_over:
@@ -37,12 +57,16 @@ while not game_over:
             game_over = True
 
     pantalla_principal.fill((0, 0, 255))
-   
+    #cuadrado.mover(800, 600)
+    #cuadrado2.mover(800, 600)
+    for a in range(len(cuadradi)):
+        cuadradi[a].mover(800, 600)
     
     
-    
-    pg.draw.rect(pantalla_principal, cuadrado.color, (cuadrado.x, cuadrado.y, cuadrado.w, cuadrado.h))
-    pg.draw.rect(pantalla_principal, cuadrado2.color, (cuadrado2.x, cuadrado2.y, cuadrado2.w, cuadrado2.h))
+    #pg.draw.rect(pantalla_principal, cuadrado.color, (cuadrado.x, cuadrado.y, cuadrado.w, cuadrado.h))
+    #pg.draw.rect(pantalla_principal, cuadrado2.color, (cuadrado2.x, cuadrado2.y, cuadrado2.w, cuadrado2.h))
+    for i in range(len(cuadradi)):
+        pg.draw.rect(pantalla_principal, cuadradi[i].color, (cuadradi[i].x, cuadradi[i].y, cuadradi[i].w, cuadradi[i].h))
     pg.display.flip()
 
 pg.quit()
